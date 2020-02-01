be57dd1e3a42e021564d8a4e4c049c23efdcf45a
Mh Hasanoghli <hasanoghli.mh@outlook.com>
2020-02-02 00:45:28 +0330

    Product Comlitaion

--- WHMS/Models/Product.cs
+++ WHMS/Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace WHMS.Models
 {
@@ -13,6 +14,13 @@ namespace WHMS.Models
         public int BrandId { get; set; }
         public Brand Brand { get; set; }
         public int PartNo { get; set; }
+        public int CurrencyUnitID { get; set; }
+        public CurrencyUnit CurrencyUnit { get; set; }
+        public int UnitID { get; set; }
+        public Unit Unit { get; set; }
+        public string Description { get; set; }
+        public int ReOrderPoint { get; set; }
+        public int Price { get; set; }
 
 
     }
